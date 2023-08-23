36f60b7bb8eb977bf8ec96c7d2ff972d379fe7f4
CallMeMo <addahri.mohamed297@gmail.com>
2023-08-23 17:01:56 +0200

    #

--- index.js
+++ index.js
@@ -15,3 +15,44 @@ listTypeForm.addEventListener("submit", function (event) {
   }
 });
  */
+document.addEventListener("DOMContentLoaded", function() {
+  const addButton = document.getElementById("add-button");
+  const todoInput = document.getElementById("todo-input");
+  const taskList = document.querySelector(".task-list");
+
+  // Add new task
+  addButton.addEventListener("click", function() {
+    const taskText = todoInput.value.trim();
+    if (taskText !== "") {
+      addTask(taskText);
+      todoInput.value = "";
+    }
+  });
+
+  // Add task when pressing Enter in the input field
+  todoInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+      addButton.click();
+    }
+  });
+
+  // Toggle task completion
+  taskList.addEventListener("click", function(event) {
+    if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+      const taskText = event.target.nextElementSibling;
+      taskText.classList.toggle("completed");
+    }
+  });
+
+  function addTask(text) {
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("task-item");
+    taskItem.innerHTML = `
+      <input type="checkbox" />
+      <span class="task-text">${text}</span>
+    `;
+    taskList.appendChild(taskItem);
+  }
+});
+
+
