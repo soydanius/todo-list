2d6cb9c09567f28bb54d9d24b6be5dc194d0b85e
Soydan Yuece <soydan.yuece@gmx.de>
2023-08-24 16:40:37 +0200

    Add most js functionality

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ listTypeForm.addEventListener("submit", function (event) {
   }
 });
  */
-document.addEventListener("DOMContentLoaded", function() {
-  const addButton = document.getElementById("add-button");
+
+document.addEventListener("DOMContentLoaded", function () {
   const todoInput = document.getElementById("todo-input");
+  const addButton = document.getElementById("add-button");
   const taskList = document.querySelector(".task-list");
 
   // Add new task
-  addButton.addEventListener("click", function() {
+  addButton.addEventListener("click", function () {
     const taskText = todoInput.value.trim();
     if (taskText !== "") {
       addTask(taskText);
@@ -30,29 +31,96 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Add task when pressing Enter in the input field
-  todoInput.addEventListener("keyup", function(event) {
+  todoInput.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
       addButton.click();
     }
   });
 
   // Toggle task completion
-  taskList.addEventListener("click", function(event) {
+  taskList.addEventListener("click", function (event) {
     if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
-      const taskText = event.target.nextElementSibling;
-      taskText.classList.toggle("completed");
+      const taskItem = event.target.parentNode;
+      taskItem.classList.toggle("completed");
+      saveTasks();
     }
   });
 
   function addTask(text) {
-    const taskItem = document.createElement("li");
+    /* const taskItem = document.createElement("li");
     taskItem.classList.add("task-item");
     taskItem.innerHTML = `
       <input type="checkbox" />
       <span class="task-text">${text}</span>
     `;
-    taskList.appendChild(taskItem);
+    taskList.appendChild(taskItem); */
+
+    taskList.innerHTML += `<li class="task-item">
+    <input type="checkbox" />
+    <span class="task-text">${text}</span>
+    <button class="delete-button">
+      <i class="fas fa-trash"></i>
+    </button>
+    </li>`;
+
+    saveTasks();
   }
+
+  // Edit task
+
+  taskList.addEventListener("click", (event) => {
+    const target = event.target.closest(".task-item > .task-text");
+    const taskText = target.innerHTML;
+    const existingInput = target.querySelector("input");
+
+    if (target && !existingInput) {
+      const taskInput = taskEditInput(taskText);
+      target.innerHTML = taskInput;
+
+      const inputElement = target.querySelector("input");
+      inputElement.addEventListener("click", (event) => {
+        event.stopPropagation();
+      });
+
+      inputElement.focus();
+
+      inputElement.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+          const newTaskText = inputElement.value;
+          target.innerHTML = newTaskText;
+          saveTasks();
+        }
+      });
+    }
+  });
+
+  function taskEditInput(initialText) {
+    return `<input type="text" value=${initialText} />`;
+  }
+
+  // Delete task
+  taskList.addEventListener("click", (event) => {
+    const target = event.target.closest(".task-item > .delete-button");
+
+    if (target) {
+      target.parentNode.remove();
+      saveTasks();
+    }
+  });
+
+  // Save tasks
+  function saveTasks() {
+    allTasks = taskList.innerHTML;
+    localStorage.setItem("savedTasks", allTasks);
+  }
+
+  // Load tasks
+  function loadSavedTasks() {
+    const savedTasks = localStorage.getItem("savedTasks");
+    taskList.innerHTML += savedTasks;
+    if (!savedTasks) {
+      taskList.innerHTML = "";
+    }
+  }
+  loadSavedTasks();
 });
-
-
